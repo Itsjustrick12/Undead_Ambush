e81ca29cee7d54c881ef8919bd5fc28606cd4397
Itsjustrick12 <Rickmjenkins900@gmail.com>
2023-10-13 16:59:25 -0400

    Updating Bullet UI

--- Assets/Scripts/UI/GrenadeUI.cs
+++ Assets/Scripts/UI/GrenadeUI.cs
@@ -5,17 +5,10 @@ using TMPro;
 
 public class GrenadeUI : MonoBehaviour
 {
-    public TextMeshProUGUI text;
     public GameObject[] GrenadeImages;
-
-    private void Start()
-    {
-        text = GetComponentInChildren<TextMeshProUGUI>();
-    }
 
     public void UpdateGrenades(int amt)
     {
-        text.text = amt.ToString();
 
         for (int i = 0; i < 3; i++)
         {
--- Assets/Scripts/UI/LandmineUI.cs
+++ Assets/Scripts/UI/LandmineUI.cs
@@ -5,18 +5,9 @@ using TMPro;
 
 public class LandmineUI : MonoBehaviour
 {
-    public TextMeshProUGUI text;
     public GameObject[] MinesImages;
-
-
-    private void Start()
-    {
-        text = GetComponentInChildren<TextMeshProUGUI>();
-    }
-
     public void UpdateMines(int amt)
     {
-        text.text = amt.ToString();
 
         for (int i = 0; i < 3; i++)
         {
